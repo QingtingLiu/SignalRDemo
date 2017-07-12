6678f8a58be143c4db9a0ab4329309db9041cf97
lqt10067 <lqt10067@ly.com>
2017-07-12 10:26:46 +0800

    drawingboard

--- DrawingBoard/Scripts/drawingboard.js
+++ DrawingBoard/Scripts/drawingboard.js
@@ -1,25 +1,21 @@
 ﻿$(function () {
-    var colors = ["yellow", "red", "green", "blue", "black", "magenta", "white", "orange"];
+    // 画布定义开始 
+    var colors = ["black", "red", "green", "blue", "yellow", "magenta", "white"];
     var canvas = $("#canvas");
     var colorElement = $("#color");
     for (var i = 0; i < colors.length; i++) {
         colorElement.append("<option value='" + (i + 1) + "'>" + colors[i] + "</option>");
     }
-
     var buttonPressed = false;
     canvas
         .mousedown(function () {
-
             buttonPressed = true;
         })
         .mouseup(function () {
-
             buttonPressed = false;
         })
         .mousemove(function (e) {
-
             if (buttonPressed) {
-
                 setPoint(e.offsetX, e.offsetY, colorElement.val());
             }
         });
@@ -27,7 +23,6 @@
     var ctx = canvas[0].getContext("2d");
 
     function setPoint(x, y, color) {
-
         ctx.fillStyle = colors[color - 1];
         ctx.beginPath();
         ctx.arc(x, y, 2, 0, Math.PI * 2);
@@ -41,18 +36,40 @@
     $("#clear").click(function () {
         clearPoints();
     });
+    // 画布定义结束
 
+    // SignalR 客户端代码 
     var hub = $.connection.drawingBoardHub;
+    hub.state.color = colorElement.val();
+    var connected = false;
+
+    colorElement.change(function () {
+        hub.state.color = $(this).val();
+    });
+
+    canvas.mousemove(function (e) {
+        if (buttonPressed && connected) {
+            hub.server.drawPoint(Math.round(e.offsetX), Math.round(e.offsetY));
+        }
+    });
+
+    $("#clear").click(function () {
+        if (connected) {
+            hub.server.clear();
+        }
+    });
+
+    // 定义客户端方法 
+    hub.client.clear = function () {
+        clearPoints();
+    };
+
     hub.client.drawPoint = function (x, y, color) {
-        ;
         setPoint(x, y, color);
-    }
+    };
 
     hub.client.update = function (points) {
-        ;
-        if (!points) {
-            return;
-        }
+        if (!points) return;
         var width = canvas.width();
         var height = canvas.height();
         for (var x = 0; x < width; x++) {
@@ -63,10 +80,9 @@
                 }
             }
         }
-    }
+    };
 
     $.connection.hub.start().done(function () {
         connected = true;
     });
-
-});
+})
